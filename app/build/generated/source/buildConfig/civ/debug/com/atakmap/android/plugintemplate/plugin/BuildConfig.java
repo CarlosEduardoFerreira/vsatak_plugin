1bad8d9966c8b8c671e0efe5b5499d4752138c6a
Carlos Ferreira <cferreira@optimusprime.ai>
2021-01-28 14:54:51 -0500

    gitignore

--- app/build/generated/source/buildConfig/civ/debug/com/atakmap/android/plugintemplate/plugin/BuildConfig.java
+++ app/build/generated/source/buildConfig/civ/debug/com/atakmap/android/plugintemplate/plugin/BuildConfig.java
@@ -8,6 +8,6 @@ public final class BuildConfig {
   public static final String APPLICATION_ID = "com.atakmap.android.plugintemplate.plugin";
   public static final String BUILD_TYPE = "debug";
   public static final String FLAVOR = "civ";
-  public static final int VERSION_CODE = 1611771952;
-  public static final String VERSION_NAME = "1.0 (e413761d) - [4.2.0]";
+  public static final int VERSION_CODE = 1611854584;
+  public static final String VERSION_NAME = "1.0 (b3fc4cdf) - [4.2.0]";
 }
